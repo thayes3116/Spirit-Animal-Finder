fa711e57f8ab1fcf2d8dd6319d63b01edee83bb3
thayes3116 <timhayes3116@gmail.com>
2017-06-29 16:52:44 -0400

    I moved all of the logic to the backend

--- app/public/app.js
+++ app/public/app.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	
 	var a1 = 0, a2 =0,a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0, a10 = 0;
 	var scoreArray = [];
-	var closest = 30;
-	var userName;
+	
+	
 
 	$("#quest1 li").on("click", function(){
 		a1 = $(this).val();				
@@ -36,8 +36,6 @@ $(document).ready(function(){
 		a10 = $(this).val();				
 	});
 	
-	
-
 	$("#submitAnswers").on("click", function(){
 
 		var scoreArray = [a1,a2,a3,a4,a5,a6,a7,a8,a9,a10];
@@ -49,67 +47,32 @@ $(document).ready(function(){
 			"scores": scoreArray
 		};
 		
-
 		$.post("/api/friends", animalObject, function(){
+
 			$("#userName").val('');
-			$("#userPhoto").val('');	
-		});
+			$("#userPhoto").val('');
 
-		matchUp(scoreArray);
+			a1 = 0, a2 =0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0, a10 = 0;	
 
-	});
+		}).then(function(match){
 
-	function matchUp(scoreArray){
-		var sumArray = [];
-		var sum = 0; 
-		var closestNumber= 0;
-		var currentURL = window.location.origin;
-
-		$.ajax(
-		{
-			url: currentURL + '/api/friends',
-			method: 'GET'
-		})
-		.done(function(friends) {
+			var parsed = JSON.parse(match)
 			
-			for(var i = 0; i < friends.length -1; i++){
-				
-				for(var j = 0; j < scoreArray.length; j++) {
-					sum += (Math.abs(scoreArray[j] - friends[i].scores[j]));								
-				}
-				
-				sumArray.push(sum);
-				sum = 0; 
-				a1 = 0, a2 =0,a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0, a10 = 0;
-				
-			}
-			
-			for (var i = 0 ; i< sumArray.length;i++){
+			$('.modal-body').empty();
+			$('.modal').modal('show');
 
-				if(sumArray[i] < closest){
-					closest = sumArray[i];
-					closestNumber = i;						
-				}
-			}
-		
-			fireModal(friends, closestNumber);
+			var closeName = parsed.name;
+			var closePhoto = parsed.photo;
+
+			var nameDiv = $('<h3>');
+			var photoDiv = $('<img id="animalImg">');
+
+			nameDiv.text(closeName);
+			photoDiv.attr('src', closePhoto);
+
+			$('.modal-body').append(nameDiv);
+			$('.modal-body').append(photoDiv);
 		});		
-	}
-
-	function fireModal(friends,closestNumber){
-		
-		$('.modal-body').empty();
-		$('.modal').modal('show');
-
-		var closeName = friends[closestNumber].name;
-		var closePhoto = friends[closestNumber].photo;
-		var nameDiv = $('<h3>');
-		var photoDiv = $('<img id="animalImg">');
-		console.log(closeName)
-		nameDiv.text(closeName);
-		photoDiv.attr('src', closePhoto);
-
-		$('.modal-body').append(nameDiv);
-		$('.modal-body').append(photoDiv);
-	}
+	});
 });
+	
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,39 @@ module.exports = function(app) {
 	});
 
 	app.post('/api/friends', function(req, res) {
+		var closest = 30;
+		var userName = req.body.name;
+		var userPhoto = req.body.photo;
+		var scoreArray = req.body.scores;
+		matchUp();
+
+		function matchUp(){
+			var sumArray = [];
+			var sum = 0; 
+			var closestNumber= 0;
 		
-		friends.push(req.body);
-		console.log(friends);
-		
-		res.json(friends);
+				for(var i = 0; i < friends.length; i++){
+					
+					for(var j = 0; j < scoreArray.length; j++) {
+						sum += (Math.abs(scoreArray[j] - friends[i].scores[j]));								
+					}
+					
+					sumArray.push(sum);
+					sum = 0; 				
+				}
+				for (var i = 0 ; i< sumArray.length;i++){
+
+					if(sumArray[i] < closest){
+						closest = sumArray[i];
+						closestNumber = i;						
+					}
+				}
+				var match = friends[closestNumber]
+				
+				friends.push(req.body);
+				
+				var string = JSON.stringify(match)
+				res.end(string);
+		};		
 	});
 }
