0aad78b5a404a1667f912e4e168ab4d0d2e51fa4
thayes3116 <timhayes3116@gmail.com>
2017-07-02 11:23:50 -0400

    fixed the animal values

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friendsArray = [{
         "name": "Racoon",
         "photo": "http://www.universeofsymbolism.com/images/raccoon-fer.jpg",
-        "scores": [3, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [3, 2, 4, 2, 3, 2, 3, 3, 2, 2]
     },
     {
         "name": "Wolf",
         "photo": "http://www.spiritanimal.info/wp-content/uploads/Wolf-Spirit-Animal-2.jpg",
-        "scores": [4, 1, 2, 2, 5, 1, 2, 5, 4, 1] 
+        "scores": [5, 1, 3, 1, 2, 1, 5, 5, 2, 1] 
     },
     {
         "name": "Bear",
         "photo": "https://www.wildgratitude.com/wp-content/uploads/2015/05/meaning-of-bear-spirit-animal.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [5, 5, 2, 5, 3, 1, 4, 3, 4, 3]
     },
     {
         "name": "Hawk",
         "photo": "http://www.universeofsymbolism.com/images/hawk-spirit-animal.jpg",
-        "scores": [1, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [2, 2, 1, 4, 5, 1, 4, 5, 5, 5]
     },
     {
         "name": "Snake",
         "photo": "https://i1.wp.com/gostica.com/wp-content/uploads/2016/12/spirit-animal-snake.jpg?fit=600%2C315",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 2]
+        "scores": [2, 4, 5, 3, 4, 4, 5, 5, 3, 4]
     },
     {
         "name": "Panda",
         "photo": "http://www.spiritanimal.info/wp-content/uploads/Panda-Spirit-Animal-1.jpg",
-        "scores": [4, 1, 1, 3, 5, 1, 2, 5, 4, 1]
+        "scores": [1, 1, 3, 1, 5, 5, 1, 1, 1, 2]
     },
     {
         "name": "Leopard",
         "photo": "https://vignette4.wikia.nocookie.net/spiritanimals/images/8/89/UrazaATT.png/revision/latest?cb=20140622024305",
-        "scores": [3, 1, 4, 4, 5, 2, 2, 5, 4, 1]
+        "scores": [3, 4, 2, 3, 4, 2, 2, 2, 4, 4]
     },
     {
         "name": "Honey Badger",
         "photo": "https://s-media-cache-ak0.pinimg.com/736x/7e/17/9e/7e179ea2c43a14e1e6c60fac2e54ba5b.jpg",
-        "scores": [2, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        "scores": [4, 5, 1, 5, 1, 5, 1, 3, 5, 5]
     },
     {
         "name": "Owl",
         "photo": "http://www.spiritanimal.info/pictures/owl/Owl-Spirit-Animal-4.jpg",
-        "scores": [1, 1, 4, 4, 3, 1, 2, 5, 4, 1]
+        "scores": [4, 2, 1, 4, 1, 1, 4, 4, 3, 1]
     },
     {
         "name": "Deer",
         "photo": "https://www.wildgratitude.com/wp-content/uploads/2014/06/deer-spirit-animal.jpg",
-        "scores": [1, 1, 4, 4, 5, 1, 2, 1, 4, 1]
+        "scores": [1, 2, 2, 1, 5, 3, 2, 1, 2, 1]
     },
 ];
 module.exports = friendsArray;
